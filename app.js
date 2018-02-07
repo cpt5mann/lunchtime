cb1449f0d5d12f37d40908d9fcfa56305e137ff3
Carter <chalfmann9547@myhta.org>
2018-02-06 14:32:46 -1000

    working on streeeetch goals

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var food=document.getElementById("food");
 var btn=document.getElementById("btn");
 var adjective=document.getElementById("adjective");
 var verb=document.getElementById("verb");
+var location=document.getElementById("locale");
 
 var objectarray=[
-{imagesrc:"https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg", food:"Tacos", adjective:"Crunchy", verb:"Tasty,"},
-{imagesrc:"https://commentform.marketforce.com/images/BK-WebComment/BB_WHOPPER-v1.png", food:"Burgers", adjective:"Typical", verb:"Amazingly"},
-{imagesrc:"http://1.bp.blogspot.com/_ZBuUcD-qFmc/TGVTpJ1Z7QI/AAAAAAAACTM/gRW6Vf5wi9s/s1600/All+Star+Sandwich+Bar+-+Cheese+Sandwich.JPG", food:"Sandwich", adjective:"Photogenic", verb:"Cool"},
-{imagesrc:"https://assets3.thrillist.com/v1/image/1159656/size/tmg-slideshow_l.jpg", food:"Pizza", adjective:"SADMEP", verb:"Overrated"}
+{imagesrc:"https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg", food:"Tacos", adjective:"Crunchy", verb:"Tasty", location:"Located at Taco Bell!" },
+{imagesrc:"https://commentform.marketforce.com/images/BK-WebComment/BB_WHOPPER-v1.png", food:"Burgers", adjective:"Typical", verb:"Amazingly", location:"Located at Burger King!"},
+{imagesrc:"http://1.bp.blogspot.com/_ZBuUcD-qFmc/TGVTpJ1Z7QI/AAAAAAAACTM/gRW6Vf5wi9s/s1600/All+Star+Sandwich+Bar+-+Cheese+Sandwich.JPG", food:"Sandwich", adjective:"Photogenic", verb:"Cool", location:"Located at Arby's!"},
+{imagesrc:"https://assets3.thrillist.com/v1/image/1159656/size/tmg-slideshow_l.jpg", food:"Pizza", adjective:"SADMEP", verb:"Overrated", location:"Located at Dominoes!"}
 ]
 
 
